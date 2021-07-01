cd5837613f0e17618266de6453d56abfe7bc8e07
Rafael Setton <rasealca2017@gmail.com>
2021-07-01 13:48:31 -0400

    Flutter Dia 2

---
+++ Flutter/payflow/lib/modules/login/login_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+import '../../shared/themes/app_images.dart';
+import '../../shared/widgets/social_login/social_login_button.dart';
+import '../../shared/themes/app_text_styles.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              height: size.height * 0.36,
+              width: size.width,
+            ),
+            Positioned(
+              child: Image.asset(
+                AppImages.person,
+                height: size.height * 0.4,
+              ),
+              left: 0,
+              right: 0,
+              top: 45,
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: size.height * 0.1,
+              child: Column(
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        horizontal: size.width * 0.2, vertical: 40),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      style: TextStyles.titleHome,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SocialLoginButton(
+                    onTap: () {
+                      print("object");
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ Flutter/payflow/lib/shared/widgets/social_login/social_login_button.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+import '../../themes/app_images.dart';
+import '../../themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final Function onTap;
+  const SocialLoginButton({Key key, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: size.height * 0.08,
+        width: size.width * 0.7,
+        decoration: BoxDecoration(
+            color: AppColors.shape,
+            border: Border.all(color: AppColors.stroke),
+            borderRadius: BorderRadius.circular(15)),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 25,
+              child: Image.asset(AppImages.google),
+            ),
+            Container(
+                width: 0,
+                decoration:
+                    BoxDecoration(border: Border.all(color: AppColors.stroke))),
+            Expanded(
+              flex: 75,
+              child: Center(
+                child: Text(
+                  "Entrar com Google",
+                  style: TextStyles.buttonGray,
+                ),
+              ),
+            ),
+            Expanded(flex: 10, child: Container()),
+          ],
+        ),
+      ),
+    );
+  }
+}
